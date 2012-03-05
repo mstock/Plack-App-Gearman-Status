73679bd6b9af3d43587d0e2264c2809a3cdf66e6
Manfred Stock <mstock@cpan.org>
2012-03-05 22:20:01 +0100

    Add a first, basically working implementation
    Also add a test for the get_status() method and some code to mock a Gearman job
    server.

--- lib/Plack/App/Gearman/Status.pm
+++ lib/Plack/App/Gearman/Status.pm
@@ -7,7 +7,10 @@ use strict;
 use warnings;
 
 use Carp;
-
+use MRO::Compat;
+use Net::Telnet::Gearman;
+use Text::MicroTemplate;
+use Plack::Util::Accessor qw(job_servers template);
 
 =head1 SYNOPSIS
 
@@ -22,6 +25,150 @@ use Carp;
 Plack::App::Gearman::Status displays the status of Gearman job servers.
 
 =cut
+
+chomp(my $template_string = <<'EOTPL');
+<?xml version="1.0"?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+	<head>
+		<title>Gearman Server Status</title>
+		<meta http-equiv="Content-Type" content="application/xhtml+xml; charset=utf-8" />
+		<style type="text/css">
+			html, body {
+				padding: 0px;
+				margin: 5px;
+				background-color: #FFFFFF;
+				font-family: Helvetica, Sans-Serif;
+			}
+			h1, h2, h3 {
+				border: solid #EAEAEA 1px;
+				padding: 5px;
+				margin-top: inherit;
+				margin-bottom: inherit;
+				background-color: #FAFAFA;
+				color: #777777;
+			}
+			h1 {
+				border-radius: 10px;
+				-moz-border-radius: 10px;
+			}
+			h2 {
+				font-size: 1.25em;
+				margin-top: 40px;
+				border-radius: 10px 10px 0px 0px;
+				-moz-border-radius: 10px 10px 0px 0px;
+			}
+			h3 {
+				font-size: 1em;
+			}
+			p {
+				margin: 5px;
+				color: #444444;
+				font-size: 0.9em;
+			}
+			table {
+				width: 100%;
+				border: 1px solid #DDDDDD;
+				border-spacing: 0px;
+			}
+			table.status {
+				border-radius: 0px 0px 10px 10px;
+				-moz-border-radius: 0px 0px 10px 10px;
+			}
+			table th {
+				border-bottom: 1px solid #DDDDDD;
+				background: #FAFAFA;
+				padding: 5px;
+				font-size: 0.9em;
+				color: #555555;
+			}
+			table td {
+				text-align: center;
+				padding: 5px;
+				font-size: 0.8em;
+				color: #444444;
+			}
+			table tr:hover {
+				background: #FBFBFB;
+			}
+		</style>
+	</head>
+	<body>
+		<h1>Gearman Server Status</h1>
+		<% for my $job_server (@{$_[0]}) { %>
+			<h2>Job server <code><%= $job_server->{job_server} %></code></h2>
+			<p>Server Version: <%= $job_server->{version} %></p>
+
+			<h3>Workers</h3>
+			<table class="workers">
+				<tr><th>File Descriptor</th><th>IP Address</th><th>Client ID</th><th>Function</th></tr>
+				<% for my $worker (@{$job_server->{workers}}) { %>
+					<tr><td><%= $worker->file_descriptor() %></td><td><%= $worker->ip_address() %></td><td><%= $worker->client_id() %></td><td><%= join(', ', sort @{$worker->functions()}) %></td></tr>
+				<% } %>
+			</table>
+
+			<h3>Status</h3>
+			<table class="status">
+				<tr><th>Function</th><th>Total</th><th>Running</th><th>Available Workers</th><th>Queue</th></tr>
+				<% for my $status (@{$job_server->{status}}) { %>
+					<tr><td><%= $status->name() %></td><td><%= $status->running() %></td></td><td><%= $status->busy() %></td><td><%= $status->free() %></td><td><%= $status->queue() %></tr>
+				<% } %>
+			</table>
+		<% } %>
+	</body>
+</html>
+EOTPL
+
+sub new {
+	my ($class, @arg) = @_;
+
+	my $self = $class->next::method(@arg);
+
+	$self->job_servers({
+		map {
+			my ($host, $port) = $_ =~ m{^\[?([\w.:]+)\]?:(\d+)$};
+			$_ => Net::Telnet::Gearman->new(
+				Host => $host,
+				Port => $port,
+			);
+		} @{$self->{job_servers}}
+	});
+
+	$self->template(Text::MicroTemplate->new(
+		template   => $template_string,
+		tag_start  => '<%',
+		tag_end    => '%>',
+		line_start => '%',
+	)->build());
+
+	return $self;
+}
+
+sub get_status {
+	my ($self) = @_;
+
+	my @result;
+	for my $job_server (keys %{$self->job_servers()}) {
+		push @result, {
+			job_server => $job_server,
+			workers    => [ $self->job_servers()->{$job_server}->workers() ],
+			status     => [ $self->job_servers()->{$job_server}->status() ],
+			version    => $self->job_servers()->{$job_server}->version(),
+		};
+	}
+
+	return \@result;
+}
+
+sub call {
+	my ($self, $env) = @_;
+
+	return [
+		200,
+		[ 'Content-Type' => 'text/html; charset=utf-8' ],
+		[ $self->template()->($self->get_status()) ]
+	];
+}
 
 1;
 
--- t/lib/Plack/App/Gearman/StatusTest.pm
+++ t/lib/Plack/App/Gearman/StatusTest.pm
@@ -7,6 +7,8 @@ use warnings;
 use Carp;
 use Test::More;
 use Test::Exception;
+use Test::TCP;
+use IO::Socket::INET;
 
 use Plack::App::Gearman::Status;
 
@@ -15,6 +17,67 @@ sub new_test: Test(1) {
 
 	my $app = Plack::App::Gearman::Status->new();
 	isa_ok($app, 'Plack::App::Gearman::Status', 'instance created');
+}
+
+sub get_status_test : Test(1) {
+	my ($self) = @_;
+
+	test_tcp(
+		client => sub {
+			my ($port) = @_;
+
+			my $app = Plack::App::Gearman::Status->new({
+				job_servers => ['127.0.0.1:'.$port],
+			});
+			is_deeply($app->get_status(), [{
+				job_server => '127.0.0.1:10001',
+				version    => '0.13',
+				status     => [{
+					busy    => 2,
+					free    => 1,
+					name    => 'add',
+					queue   => 1,
+					running => 3
+				}],
+				workers    => [{
+					client_id       => '-',
+					file_descriptor => 8432,
+					functions       => [ 'job' ],
+					ip_address      => '192.168.0.1'
+				}]
+			}], 'status ok');
+		},
+		server => sub {
+			my ($port) = @_;
+			$self->mock_gearman($port);
+		}
+	);
+}
+
+
+sub mock_gearman {
+	my ($self, $port) = @_;
+
+	my $sock = IO::Socket::INET->new(
+		Listen    => 5,
+		LocalAddr => 'localhost',
+		LocalPort => $port,
+		Proto     => 'tcp',
+		ReuseAddr => 1,
+	);
+	while (my $res = $sock->accept()) {
+		while (my $line = $res->getline()) {
+			if (index($line, 'workers') == 0) {
+				$res->print("8432 192.168.0.1 - : job\n.\n");
+			}
+			elsif (index($line, 'status') == 0) {
+				$res->print("add 1       2       3\n.\n");
+			}
+			elsif (index($line, 'version') == 0) {
+				$res->print("0.13\n");
+			}
+		}
+	}
 }
 
 1;
